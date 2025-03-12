2867ff27bd9cc181e6c7ddcaf8114760c0a4b872
Kevin Morales <kfmorales94@gmail.com>
2025-03-11 23:47:57 -0500

    Remove unused

--- app/src/main/java/com/kevinhomorales/vertexaikotlinjetpackcompose/helpers/VertexAIHelper.kt
+++ app/src/main/java/com/kevinhomorales/vertexaikotlinjetpackcompose/helpers/VertexAIHelper.kt
@@ -1,16 +1,11 @@
 package com.kevinhomorales.vertexaikotlinjetpackcompose.helpers
 
-import android.content.Context
 import com.google.firebase.Firebase
-import com.google.firebase.FirebaseApp
 import com.google.firebase.vertexai.vertexAI
 
 class VertexAIHelper {
-
-    // Inicializar el modelo de Vertex AI en Firebase
     private val vertexAI = Firebase.vertexAI
     private val model = vertexAI.generativeModel("gemini-2.0-flash")
-
     suspend fun generateTextFromGemini(prompt: String): String {
         return try {
             val response = model.generateContent(prompt)
